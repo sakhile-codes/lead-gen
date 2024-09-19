02280f1250682a2b63fee7dd46f804490a29b288
smlambo023 <smlambo023@student.wethinkcode.co.za>
2024-09-19 13:17:37 +0200

    get location coordinates by name

--- app.py
+++ app.py
@@ -10,3 +10,12 @@ app = Flask(__name__)
 # Google Maps API client
 api_key = 'YOUR_GMAPS_API_KEY_HERE'  # Replace with your actual Google API key
 gmaps = googlemaps.Client(key=api_key)
+
+# Function to convert location name to coordinates
+def get_coordinates(location_name):
+    geocode_result = gmaps.geocode(location_name)
+    if geocode_result:
+        lat = geocode_result[0]['geometry']['location']['lat']
+        lon = geocode_result[0]['geometry']['location']['lng']
+        return lat, lon
+    return None, None
