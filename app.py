bc13d5eb328d9651145d03edd85a444ae1675e68
smlambo023 <smlambo023@student.wethinkcode.co.za>
2024-09-19 13:18:29 +0200

    business list

--- app.py
+++ app.py
@@ -19,3 +19,30 @@ def get_coordinates(location_name):
         lon = geocode_result[0]['geometry']['location']['lng']
         return lat, lon
     return None, None
+
+# Helper function to extract business data from API results
+def extract_businesses(places_result):
+    business_list = []
+    for place in places_result['results']:
+        business = {
+            'name': place['name'],
+            'address': place.get('vicinity', 'N/A'),
+            'rating': place.get('rating', 'N/A'),
+            'place_id': place['place_id']
+        }
+        
+        # Get additional business details like phone number and website
+        details = get_business_details(place['place_id'])
+        business['phone_number'] = details['phone_number']
+        business['website'] = details['website']
+        business_list.append(business)
+    return business_list
+
+# Function to get detailed business information using place_id
+def get_business_details(place_id):
+    place_details = gmaps.place(place_id=place_id, fields=['name', 'formatted_phone_number', 'website'])
+    details = {
+        'phone_number': place_details['result'].get('formatted_phone_number', 'N/A'),
+        'website': place_details['result'].get('website', 'N/A')
+    }
+    return details
